c5be53f0df8b2082135274a54610a5011adc9e01
OnoFumiya <fumiyaono.choi@gmail.com>
2025-11-03 20:53:01 +0900

    launch include parameter

--- launch/swerve_controller.launch.py
+++ launch/swerve_controller.launch.py
@@ -9,7 +9,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import PathJoinSubstitution
@@ -19,11 +21,15 @@ from launch.substitutions.launch_configuration import LaunchConfiguration
 
 def generate_launch_description():
     arg_robot_name = DeclareLaunchArgument('robot_name', default_value='sobit_home')
-    arg_enable_gz  = DeclareLaunchArgument('enable_gz', default_value='True')
+    arg_enable_gz  = DeclareLaunchArgument('enable_gz', default_value='False')
+    arg_config     = DeclareLaunchArgument('config', 
+        default_value=os.path.join(get_package_share_directory('swerve_steering_controller'), 'config', "swerve.yaml"
+    ))
 
     return LaunchDescription([
         arg_robot_name,
         arg_enable_gz,
+        arg_config,
         OpaqueFunction(function = launch_gz),
     ])
 
@@ -31,14 +37,7 @@ def generate_launch_description():
 def launch_gz(context, *args, **kwargs):
     robot_name = LaunchConfiguration('robot_name').perform(context)
     enable_gz  = LaunchConfiguration('enable_gz').perform(context)
-
-    config = PathJoinSubstitution(
-        [
-            FindPackageShare("swerve_steering_controller"),
-            "config",
-            "swerve.yaml",
-        ]
-    )
+    config = LaunchConfiguration('config').perform(context)
 
     # convert enable_gz string to boolean for use_sim_time
     use_sim_time = str(enable_gz).lower() in ['true', '1', 'yes']
